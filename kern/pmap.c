fe891a461fa8c82f6446632c618f02832b97f10a
runshenzhu <runshen.zhu@gmail.com>
2015-04-09 20:28:10 +0800

    minor update

--- kern/pmap.c
+++ kern/pmap.c
@@ -156,11 +156,14 @@ mem_init(void)
 	// Your code goes here:
 	size_t pages_size = npages * sizeof(struct PageInfo);
 	pages = (struct PageInfo *) boot_alloc(pages_size);
-	memset(pages, 0, npages * sizeof(struct PageInfo));
+	memset(pages, 0, pages_size);
 
 	//////////////////////////////////////////////////////////////////////
 	// Make 'envs' point to an array of size 'NENV' of 'struct Env'.
 	// LAB 3: Your code here.
+	size_t envs_size = NENV * sizeof(struct Env);
+	envs = (struct Env *)boot_alloc(envs_size);
+	memset(envs, 0, envs_size);
 
 	//////////////////////////////////////////////////////////////////////
 	// Now that we've allocated the initial kernel data structures, we set
@@ -194,6 +197,7 @@ mem_init(void)
 	//    - the new image at UENVS  -- kernel R, user R
 	//    - envs itself -- kernel RW, user NONE
 	// LAB 3: Your code here.
+	boot_map_region(kern_pgdir, UENVS, ROUNDUP(envs_size, PGSIZE), PADDR(envs), PTE_U);
 
 	//////////////////////////////////////////////////////////////////////
 	// Use the physical memory that 'bootstack' refers to as the kernel
